f86ca8cad29d15ec54aef235f3ac41489cfb49d0
JP-1907 <juanpablorojasvargas07@gmail.com>
2025-11-11 17:00:09 -0400

    Update producto.py

--- producto.py
+++ producto.py
@@ -8,11 +8,22 @@ class Producto:
     def __str__(self):
         return f"{self.nombre} - Precio: {self.precio:.2f} - Cantidad: {self.cantidad}"
 
-class Salchicha(Producto):
+class Perro:
+    def __init__(self, nombre, pan, salchicha, toppings, salsas, acompañantes):
+        self.nombre = nombre
+        self.pan = pan
+        self.salchicha = salchicha
+        self.toppings = toppings
+        self.salsas = salsas
+        self.acompañantes = acompañantes
+class PerroProducto(Producto):
     def __init__(self, id, nombre, precio, cantidad, es_vegano):
         super().__init__(id, nombre, precio, cantidad)
         self.es_vegano = es_vegano
 
+    def __str__(self):
+        tipo = "Vegano" if self.es_vegano else "No vegano"
+        return f"{super().__str__()} - Tipo: {tipo}"
 class Pan(Producto):
     def __init__(self, id, nombre, precio, cantidad, tamaño, es_integral):
         super().__init__(id, nombre, precio, cantidad)
