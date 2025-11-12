d82bfbfb22c12045d1073719d932ea4f7d19edd7
JP-1907 <juanpablorojasvargas07@gmail.com>
2025-11-12 08:44:02 -0400

    Update producto.py

--- producto.py
+++ producto.py
@@ -17,12 +17,9 @@ class Perro:
         self.salsas = salsas
         self.acompañantes = acompañantes
 class PerroProducto(Producto):
-    def __init__(self, id, nombre, precio, cantidad, es_vegano):
+    def __init__(self, id, nombre, precio, cantidad):
         super().__init__(id, nombre, precio, cantidad)
-        self.es_vegano = es_vegano
-
     def __str__(self):
-        tipo = "Vegano" if self.es_vegano else "No vegano"
         return f"{super().__str__()} - Tipo: {tipo}"
 class Pan(Producto):
     def __init__(self, id, nombre, precio, cantidad, tamaño):
@@ -30,12 +27,11 @@ class Pan(Producto):
         self.tamaño = tamaño
 
 class Refresco(Producto):
-    def __init__(self, id, nombre, precio, cantidad, sabor, contiene_azucar):
+    def __init__(self, id, nombre, precio, cantidad):
         super().__init__(id, nombre, precio, cantidad)
-        self.sabor = sabor
-        self.contiene_azucar = contiene_azucar
 
     def es_saludable(self):
 
         return not self.contiene_azucar
+
 
