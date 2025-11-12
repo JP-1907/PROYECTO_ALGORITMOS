6c2040e1be804dba4b53dd692ea7d077c7e7256e
JP-1907 <juanpablorojasvargas07@gmail.com>
2025-11-12 11:32:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
-import funciones as f
-import json
-
-# Cargar el menú desde ingredientes.json
-try:
-    with open("ingredientes.json", "r", encoding="utf-8") as file:
-        menu_json = json.load(file)
-except FileNotFoundError:
-    print("Error: No se encontró el archivo ingredientes.json.")
-    menu_json = []
-
-# Transformar el menú en objetos de producto
-productos = f.transformar_menu(menu_json)
-clientes = []
-
 def main():
     print("***********************************************************************")
     print("Te damos la bienvenida al sistema de gestión de Hot Dog CCS")
     print("***********************************************************************\n")
 
     while True:
-        print("\n====Menú principal====")
+        print("\n====Menú Principal====")
         print("1. Registrar cliente")
-        print("2. Mostrar menú")
+        print("2. Mostrar menú (Productos disponibles para la venta)")
         print("3. Realizar compra")
-        print("4. Salir")
+        print("4. Gestión de Inventario") 
+        print("5. Salir") 
 
         opcion = input("Ingrese la opción deseada: ")
 
@@ -36,14 +22,15 @@ def main():
                 print("No hay productos disponibles.")
         elif opcion == "3":
             if productos:
-                f.realizar_compra(clientes, productos)
+                f.realizar_compra(clientes, productos) 
             else:
                 print("No se puede realizar la compra: menú vacío.")
         elif opcion == "4":
+            menu_inventario(inventario)
+        elif opcion == "5":
             print("Saliendo del sistema...")
             break
         else:
             print("Opción no válida. Intente nuevamente.")
 
-main()
 
