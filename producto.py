c41a66e94aecc78dbbbdb68aa268625dea73884f
JP-1907 <juanpablorojasvargas07@gmail.com>
2025-11-13 23:19:49 -0400

    Update producto.py

--- producto.py
+++ producto.py
@@ -17,27 +17,53 @@ class Perro:
         self.salsas = salsas
         self.acompañantes = acompañantes
 class PerroProducto(Producto):
-    def __init__(self, id, nombre, precio, cantidad, es_vegano):
+    def __init__(self, id, nombre, precio, cantidad, tipo_salchicha="vienesa"):
         super().__init__(id, nombre, precio, cantidad)
-        self.es_vegano = es_vegano
+        self.tipo_salchicha = tipo_salchicha
 
     def __str__(self):
-        tipo = "Vegano" if self.es_vegano else "No vegano"
-        return f"{super().__str__()} - Tipo: {tipo}"
+        return 
+
+    def __str__(self):
+        return f"{super().__str__()} - Tipo de salchicha: {self.tipo_salchicha}"
+
 class Pan(Producto):
-    def __init__(self, id, nombre, precio, cantidad, tamaño, es_integral):
+    def __init__(self, id, nombre, precio, cantidad, tamaño):
         super().__init__(id, nombre, precio, cantidad)
         self.tamaño = tamaño
-        self.es_integral = es_integral
 
-class Refresco(Producto):
-    def __init__(self, id, nombre, precio, cantidad):
+    def __str__(self):
+        return f"{super().__str__()} - Tamaño: {self.tamaño}"
+
+
+
+
+class Topping(Producto):
+    def __init__(self, id, nombre, precio, cantidad, es_picante=False):
         super().__init__(id, nombre, precio, cantidad)
+        self.es_picante = es_picante
 
+    def __str__(self):
+        picante = "Picante" if self.es_picante else "No picante"
+        return f"{super().__str__()} - {picante}"
 
-    def es_saludable(self):
+class Salsa(Producto):
+    def __init__(self, id, nombre, precio, cantidad, tipo="cremosa"):
+        super().__init__(id, nombre, precio, cantidad)
+        self.tipo = tipo 
 
-        return not self.contiene_azucar
+    def __str__(self):
+        return f"{super().__str__()} - Tipo de salsa: {self.tipo}"
+
+class Acompañamiento(Producto):
+    def __init__(self, id, nombre, precio, cantidad, tipo="bebida", detalles=None):
+        super().__init__(id, nombre, precio, cantidad)
+        self.tipo = tipo  # Ejemplo: bebida, papas, jugo, postre
+        self.detalles = detalles if detalles else []  # Ejemplo: ["limón", "hielo"]
+
+    def __str__(self):
+        detalles_str = ", ".join(self.detalles) if self.detalles else "Sin detalles"
+        return f"{super().__str__()} - Tipo: {self.tipo} - Detalles: {detalles_str}"
 
 
 
